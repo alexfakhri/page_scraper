95577022dd986bf4c9d29d399ac7db8988b2023d
Alex Fakhri <alexfakhri@me.com>
2017-01-14 15:38:53 +0000

    Added form to input URL and saved it in sessions

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
 
 class PageScraper < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    'Hello PageScraper!'
+    erb :index
+  end
+
+  post '/url' do
+    session[:url] = params[:url]
+    redirect :result
+  end
+
+  get '/result' do
+    @url = session[:url]
+    erb :result
   end
 
   # start the server if ruby file executed directly
---
+++ spec/features/enter_url_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'Enter URL' do
+
+  scenario 'submitting a URL' do
+    visit '/'
+    fill_in :url, with: '​http://aakashjapi.com/'
+    click_button 'Get Info'
+    expect(page).to have_content '​http://aakashjapi.com/'
+  end
+
+end
